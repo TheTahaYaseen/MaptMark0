5973b31be59574d799e006635dd2c3dc024b9792
thetahayaseen <thetahayaseen>
2023-10-13 06:56:03 -0700

    Creating The Database

--- database_schema.py
+++ database_schema.py
@@ -11,11 +11,29 @@ Tasks >
     Task
     Details
     Deadline
-    Created By
+    Created By 
 
-Photos >
+Medias >
     Id (Auto-Increment)
     Task Id (Foreign Key)
-
+    Media Source 
+    
 """
 
+import sqlite3
+
+database_connection = sqlite3.connect("mapt_database.db")
+database_cursor = database_connection.cursor()
+
+database_connection.execute("PRAGMA foreign_keys = ON")
+
+create_users_table_query = f"CREATE TABLE users(id int primary key auto increment, username varchar(255) not null, email varchar(255) not null, password varchar(255) not null)"
+create_tasks_table_query = f"CREATE TABLE tasks(id int primary key auto increment, task varchar(255) not null, details varchar(2550) not null, deadline datetime not null, state varchar(255) not null, created_by int, foreign key (created_by) references users(id))"
+create_medias_table_query = f"CREATE TABLE medias(id int primary key auto increment, media_of int, media_source varchar(2550) not null, foreign key (media_of) references tasks(id))"
+
+database_cursor.execute(create_users_table_query)
+database_cursor.execute(create_tasks_table_query)
+database_cursor.execute(create_medias_table_query)
+
+database_connection.commit()
+database_connection.close()
