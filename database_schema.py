5839bd7d5d11bca4adb98e1bf5a36463dbe2adca
thetahayaseen <thetahayaseen>
2023-10-13 06:58:37 -0700

    Fixed The Database Queries

--- database_schema.py
+++ database_schema.py
@@ -27,9 +27,9 @@ database_cursor = database_connection.cursor()
 
 database_connection.execute("PRAGMA foreign_keys = ON")
 
-create_users_table_query = f"CREATE TABLE users(id int primary key auto increment, username varchar(255) not null, email varchar(255) not null, password varchar(255) not null)"
-create_tasks_table_query = f"CREATE TABLE tasks(id int primary key auto increment, task varchar(255) not null, details varchar(2550) not null, deadline datetime not null, state varchar(255) not null, created_by int, foreign key (created_by) references users(id))"
-create_medias_table_query = f"CREATE TABLE medias(id int primary key auto increment, media_of int, media_source varchar(2550) not null, foreign key (media_of) references tasks(id))"
+create_users_table_query = f"CREATE TABLE users(id integer primary key autoincrement, username varchar(255) not null, email varchar(255) not null, password varchar(255) not null)"
+create_tasks_table_query = f"CREATE TABLE tasks(id integer primary key autoincrement, task varchar(255) not null, details varchar(2550) not null, deadline datetime not null, state varchar(255) not null, created_by integer, foreign key (created_by) references users(id))"
+create_medias_table_query = f"CREATE TABLE medias(id integer primary key autoincrement, media_of integer, media_source varchar(2550) not null, foreign key (media_of) references tasks(id))"
 
 database_cursor.execute(create_users_table_query)
 database_cursor.execute(create_tasks_table_query)
