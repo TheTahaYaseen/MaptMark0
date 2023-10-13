36d1a8419200e6e78f69f5578a8c3b7a8a605544
thetahayaseen <thetahayaseen>
2023-10-13 06:38:39 -0700

    Setting Up The App

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class MainApp(App):
+    pass
+
+class RegistrationLayout(BoxLayout):
+    pass
+
+if __name__ == "__main__":
+    MainApp().run()
