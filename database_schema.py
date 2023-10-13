ceb9de8f71683e46e97bca96826682c33748cebd
thetahayaseen <thetahayaseen>
2023-10-13 06:39:39 -0700

    Pseudocoding The Database Schema

---
+++ database_schema.py
@@ -0,0 +1,21 @@
+"""
+
+Users >
+    Id (Auto-Increment)
+    Username
+    Email
+    Password
+
+Tasks >
+    Id (Auto-Increment)
+    Task
+    Details
+    Deadline
+    Created By
+
+Photos >
+    Id (Auto-Increment)
+    Task Id (Foreign Key)
+
+"""
+
