734c23eda4c31af4afef9b9dd420469b0604e36d
thetahayaseen <thetahayaseen>
2023-10-13 07:09:43 -0700

    Added Variables To Main.kv And Organized Main.py + Also, Got SpaceGrotesk Font

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from kivy.uix.boxlayout import BoxLayout
 class MainApp(App):
     pass
 
+class MainLayout(BoxLayout):
+    pass
+
 class RegistrationLayout(BoxLayout):
+    pass
+
+class LoginLayout(BoxLayout):
     pass
 
 if __name__ == "__main__":
